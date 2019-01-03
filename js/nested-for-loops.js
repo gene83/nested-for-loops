d1dd3c9df64dc196c9462dc523182101092d4aed
Gene83 <gene83@protonmail.com>
2019-01-03 11:24:14 -1000

    tests passing

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -1,15 +1,20 @@
 /**
- * Returns a string representation of a 2-dimensional data structure 
- * @param {number} depth 
- * @param {number} [width=depth] 
+ * Returns a string representation of a 2-dimensional data structure
+ * @param {number} depth
+ * @param {number} [width=depth]
  * @returns {string}
  */
-function nestedForLoops (depth, width = depth) {
+function nestedForLoops(depth, width = depth) {
   let result = "\n";
-
-  // Write code here
-
-};
+  for (let i = 0; i < depth; i++) {
+    for (let j = 0; j < width; j++) {
+      result += `{x:${j}, y:${i}}, `;
+    }
+    result = result.slice(0, result.length - 2);
+    result += "\n";
+  }
+  return result;
+}
 
 // To see your console output outside the tests add function calls here.
 // console.log(nestedForLoops(4));
